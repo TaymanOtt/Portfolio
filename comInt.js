60473937c5aef9a215424d2552710a7964095b66
TaymanOtt <taymantott@gmail.com>
2020-09-03 16:12:09 -0500

    added madspots and changed functions

--- comInt.js
+++ comInt.js
@@ -1,4 +1,6 @@
-function compInCal(){
+let x = document.getElementById('comButt');
+x.addEventListener('click',
+function(){
     var initIn = document.getElementById("initInvest").value;
      var intRate = document.getElementById("intRate").value;
       var lenTime = document.getElementById("lentOT").value;
@@ -6,4 +8,4 @@ var CompInt =  initIn*(Math.pow((1+intRate),lenTime)+1);
 
 document.getElementById("output2").innerHTML = CompInt + " is your interest compounded.";
 
-      }
+}, false);
